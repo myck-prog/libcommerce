4cd9fcb8683b1a6ae790261939031310162ff799
myck-prog <myckland.learnprog@gmail.com>
2022-02-04 01:39:07 -0800

    final js version

--- books.js
+++ books.js
@@ -1,8 +1,43 @@
+let books;
 
-function renderBooks() {
+async function renderBooks(filter) {
   const booksWrapper = document.querySelector('.books');
+  const books = await getBooks();
 
-  const books = getBooks();
+  booksWrapper.classList += ' books__loading'
+
+  if (!books) {
+    books = await getBooks();
+  }
+  
+  booksWrapper.classList.remove('books__loading')
+
+
+  if (filter === 'LOW_TO_HIGH') {
+    books.sort((a, b) => (a.salePrice||a.originalPrice)- (b.salePrice||b.originalPrice));
+  }
+  else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a, b) => (b.salePrice||b.originalPrice) - (a.salePrice||a.originalPrice));
+  }
+  else if (filter === 'RATING') {
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
+  function ratingsHTML(rating) {
+    let ratingHTML = "";
+
+    for (let i = 0; i < Math.floor(rating); ++i) {
+      ratingHTML += '<i class="fas fa-star"></i>'
+    }
+
+    if (!Number.isInteger(rating)) {
+      ratingHTML += ' <i class="fas fa-star-half-alt"></i>'
+    }
+    return ratingHTML;
+  }
+
+
+
   const booksHtml = books.map((book) => {
     return `<div class="book">
     <figure class="book__img--wrapper">
@@ -12,23 +47,34 @@ function renderBooks() {
       ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+    ${priceHTML(book.originalPrice,book.salePrice)}
+</div>
+      
     </div>
   </div>`
   })
-  .join('');
+    .join('');
   // join for spaces
 
   // booksWrapper.innerHTML = 
   booksWrapper.innerHTML = booksHtml;
-  console.log(booksHtml)
+}
+
+function priceHTML(originalPrice,salePrice){
+  if(!salePrice){
+    return `${originalPrice.toFixed(2)}`
+  }
+  return `<span class = "book__price--normal">$${originalPrice.toFixed(2)} </span>$${salePrice}` 
+
+}
+// event properties
+function filterBooks(event) {
+
+  renderBooks(event.target.value);
+
 }
 // JS usually will render immediately. We need to wrap them first
 setTimeout(() => {
@@ -37,94 +83,98 @@ setTimeout(() => {
 
 // FAKE DATA
 function getBooks() {
-  return [
-    {
-      id: 1,
-      title: "Crack the Coding Interview",
-      url: "assets/crack the coding interview.png",
-      originalPrice: 49.95,
-      salePrice: 14.95,
-      rating: 4.5,
-    },
-    {
-      id: 2,
-      title: "Atomic Habits",
-      url: "assets/atomic habits.jpg",
-      originalPrice: 39,
-      salePrice: null,
-      rating: 5,
-    },
-    {
-      id: 3,
-      title: "Deep Work",
-      url: "assets/deep work.jpeg",
-      originalPrice: 29,
-      salePrice: 12,
-      rating: 5,
-    },
-    {
-      id: 4,
-      title: "The 10X Rule",
-      url: "assets/book-1.jpeg",
-      originalPrice: 44,
-      salePrice: 19,
-      rating: 4.5,
-    },
-    {
-      id: 5,
-      title: "Be Obsessed Or Be Average",
-      url: "assets/book-2.jpeg",
-      originalPrice: 32,
-      salePrice: 17,
-      rating: 4,
-    },
-    {
-      id: 6,
-      title: "Rich Dad Poor Dad",
-      url: "assets/book-3.jpeg",
-      originalPrice: 70,
-      salePrice: 12.5,
-      rating: 5,
-    },
-    {
-      id: 7,
-      title: "Cashflow Quadrant",
-      url: "assets/book-4.jpeg",
-      originalPrice: 11,
-      salePrice: 10,
-      rating: 4,
-    },
-    {
-      id: 8,
-      title: "48 Laws of Power",
-      url: "assets/book-5.jpeg",
-      originalPrice: 38,
-      salePrice: 17.95,
-      rating: 4.5,
-    },
-    {
-      id: 9,
-      title: "The 5 Second Rule",
-      url: "assets/book-6.jpeg",
-      originalPrice: 35,
-      salePrice: null,
-      rating: 4,
-    },
-    {
-      id: 10,
-      title: "Your Next Five Moves",
-      url: "assets/book-7.jpg",
-      originalPrice: 40,
-      salePrice: null,
-      rating: 4,
-    },
-    {
-      id: 11,
-      title: "Mastery",
-      url: "assets/book-8.jpeg",
-      originalPrice: 30,
-      salePrice: null,
-      rating: 4.5,
-    },
-  ];
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          title: "Crack the Coding Interview",
+          url: "assets/crack the coding interview.png",
+          originalPrice: 49.95,
+          salePrice: 14.95,
+          rating: 4.5,
+        },
+        {
+          id: 2,
+          title: "Atomic Habits",
+          url: "assets/atomic habits.jpg",
+          originalPrice: 39,
+          salePrice: null,
+          rating: 5,
+        },
+        {
+          id: 3,
+          title: "Deep Work",
+          url: "assets/deep work.jpeg",
+          originalPrice: 29,
+          salePrice: 12,
+          rating: 5,
+        },
+        {
+          id: 4,
+          title: "The 10X Rule",
+          url: "assets/book-1.jpeg",
+          originalPrice: 44,
+          salePrice: 19,
+          rating: 4.5,
+        },
+        {
+          id: 5,
+          title: "Be Obsessed Or Be Average",
+          url: "assets/book-2.jpeg",
+          originalPrice: 32,
+          salePrice: 17,
+          rating: 4,
+        },
+        {
+          id: 6,
+          title: "Rich Dad Poor Dad",
+          url: "assets/book-3.jpeg",
+          originalPrice: 70,
+          salePrice: 12.5,
+          rating: 5,
+        },
+        {
+          id: 7,
+          title: "Cashflow Quadrant",
+          url: "assets/book-4.jpeg",
+          originalPrice: 11,
+          salePrice: 10,
+          rating: 4,
+        },
+        {
+          id: 8,
+          title: "48 Laws of Power",
+          url: "assets/book-5.jpeg",
+          originalPrice: 38,
+          salePrice: 17.95,
+          rating: 4.5,
+        },
+        {
+          id: 9,
+          title: "The 5 Second Rule",
+          url: "assets/book-6.jpeg",
+          originalPrice: 35,
+          salePrice: null,
+          rating: 2,
+        },
+        {
+          id: 10,
+          title: "Your Next Five Moves",
+          url: "assets/book-7.jpg",
+          originalPrice: 40,
+          salePrice: null,
+          rating: 4,
+        },
+        {
+          id: 11,
+          title: "Mastery",
+          url: "assets/book-8.jpeg",
+          originalPrice: 30,
+          salePrice: null,
+          rating: 4.5,
+        },
+      ]);
+    }, 1000);
+  });
 }
